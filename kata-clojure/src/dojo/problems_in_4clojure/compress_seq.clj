16c2de44503cc0edcfbe13a1a282daee16f2ec62
jinhye <wlsgp012@naver.com>
2019-12-05 18:58:32 +0900

    Add http://www.4clojure.com/problem/30

---
+++ kata-clojure/src/dojo/problems_in_4clojure/compress_seq.clj
@@ -0,0 +1,20 @@
+(ns dojo.problems-in-4clojure.compress-seq)
+;http://www.4clojure.com/problem/30
+
+;(defn sol [col] (reduce #(if (= (last %1) %2) (conj %1 %2) %1) [] col))
+(defn sol [col]
+  (reduce
+    #(if (= (last %1) %2)
+       %1
+       (conj %1 %2))
+    []
+    col))
+
+(println (sol "Leeeeeerrroyyy"))
+
+(println (= (apply str (sol "Leeeeeerrroyyy")) "Leroy"))
+(println (= (sol [1 1 2 3 3 2 2 3]) '(1 2 3 2 3)))
+(println (= (sol [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2])))
+
+;other solutions
+#(map first (partition-by identity %))
