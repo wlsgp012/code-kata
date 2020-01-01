526d4ff6fff42454c2d70d4fe79296a2ab4adee4
jinhye <wlsgp012@naver.com>
2020-01-01 10:40:42 +0900

    Add problem 33, 41 of 4clojure.com

---
+++ kata-clojure/src/dojo/problems_in_4clojure/drop_every_nth_item_41.clj
@@ -0,0 +1,11 @@
+(ns dojo.problems-in-4clojure.drop-every-nth-item-41)
+; http://www.4clojure.com/problem/41
+
+(defn sol [col n] (flatten (partition-all (- n 1) n col)))
+
+(println (= (sol [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8]))
+(println (= (sol [:a :b :c :d :e :f] 2) [:a :c :e]))
+(println (= (sol [1 2 3 4 5 6] 4) [1 2 3 5 6]))
+
+;other solution
+;#(apply concat (partition-all (dec %2) %2 %))
---
+++ kata-clojure/src/dojo/problems_in_4clojure/replicate_a_sequence_33.clj
@@ -0,0 +1,12 @@
+(ns dojo.problems-in-4clojure.replicate-a-sequence-33)
+; http://www.4clojure.com/problem/33
+
+(defn sol [coll n] (mapcat #(repeat n %) coll))
+
+(println (sol [1 2 3] 2))
+
+(println (= (sol [1 2 3] 2) '(1 1 2 2 3 3)))
+(println (= (sol [:a :b] 4) '(:a :a :a :a :b :b :b :b)))
+
+; other solution
+; (fn [x t] (mapcat (partial repeat t) x))
