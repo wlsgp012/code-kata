08d4bcb0bc3a7f911d239826855236e9e4ebe2c3
jinhye <wlsgp012@naver.com>
2021-05-15 22:31:21 +0900

    kotlin in action p.111

---
+++ kata-kotlin/src/main/kotlin/dojo/inaction/README.md
@@ -0,0 +1,2 @@
+### Kotlin in Action
+- https://book.naver.com/bookdb/book_detail.nhn?bid=12685155
---
+++ kata-kotlin/src/main/kotlin/dojo/inaction/ch3/JoinToString.kt
@@ -0,0 +1,18 @@
+package dojo.inaction.ch3
+
+
+fun <T> joinToString(collection: Collection<T>, separator: String=", ", prefix: String="", postfix: String=""): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main() {
+    val list = listOf(1, 2, 3)
+    println(joinToString(list, "; ", "(", ")"))
+    println(joinToString(list))
+}
