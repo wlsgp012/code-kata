29554ac37eef9ca81f9b3ab472d8b0bc5774a89d
jinhye <wlsgp012@naver.com>
2021-05-17 22:03:56 +0900

    kotlin in action: ch4

---
+++ kata-kotlin/src/main/kotlin/dojo/inaction/ch4/ObjectAndCompanion.kt
@@ -0,0 +1,27 @@
+package dojo.inaction.ch4
+
+data class Person(val name: String) {
+    object NameComparator: Comparator<Person>{
+        override fun compare(o1: Person, o2: Person): Int = o1.name.compareTo(o2.name)
+    }
+}
+
+class A {
+    companion object {
+        fun bar(){
+            println("Companion object called")
+        }
+    }
+}
+
+class UserO private constructor(val nickname: String){
+    companion object{
+        fun newSubscribingUser(email: String) = UserO(email.substringBefore('@'))
+    }
+}
+
+fun main() {
+    val persons = listOf(Person("Bob"), Person("Alice"))
+    println(persons.sortedWith(Person.NameComparator))
+    A.bar()
+}
