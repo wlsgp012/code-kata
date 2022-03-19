4bbd6da202c4dda2054fd37097fd6e5a2f333376
jinhye <wlsgp012@naver.com>
2022-03-19 11:13:55 +0900

    Add 4clojure 82 - 1st try

--- kata-clojure/src/dojo/problems_in_4clojure/082_word_chains.clj
+++ kata-clojure/src/dojo/problems_in_4clojure/082_word_chains.clj
@@ -1,9 +1,15 @@
 (ns dojo.problems-in-4clojure.082-word-chains)
 ;; https://4clojure.oxal.org/#/problem/82
 
+;; wrong answer
 (defn sol
   [words]
-  )
+  (letfn [(intersection [a b] (clojure.set/intersection (set a) (set b)))
+          (difference [a b] (clojure.set/difference (set a) (set b)))
+          (a' [a b] (difference a (intersection a b)))
+          (abscount [a b] (Math/abs (count (a' a b))))
+          (linked [a b] (= 1 (abscount a b) (abscount b a)))]
+    (every? #(= 1 (count (filter (fn [x] (linked % x)) words))) words)))
 
 (= true (sol #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"}))
 (= false (sol #{"cot" "hot" "bat" "fat"}))
