b60bdff017e96402b7f7292cba02521dddbe30d6
jinhye <wlsgp012@naver.com>
2022-03-25 16:21:46 +0900

    Test constructor(main, sub)

--- kata-kotlin/src/main/kotlin/dojo/inaction/ch4/Cons.kt
+++ kata-kotlin/src/main/kotlin/dojo/inaction/ch4/Cons.kt
@@ -1,16 +1,31 @@
 package dojo.inaction.ch4
 
-class Secretive private constructor() {}
+class Secretive private constructor()
 
 open class Cons {
     constructor(ctx: String)
     constructor(ctx: String, attr: Int)
 }
 
-class MyButton: Cons {
-    constructor(ctx: String): this(ctx, 1) {}
-//    constructor(ctx: String): super(ctx) {}
-    constructor(ctx: String, attr: Int): super(ctx, attr) {
+class MyButton : Cons {
+    constructor(ctx: String) : this(ctx, 1) {}
+
+    //    constructor(ctx: String): super(ctx) {}
+    constructor(ctx: String, attr: Int) : super(ctx, attr) {
         //
     }
+}
+
+class Person2(val name: String, val registered: Int = 1) {
+    constructor(name: String) : this(name.toString(), 2) {
+        println("sub")
+    }
+
+    init {
+        println("main")
+    }
+}
+
+fun main() {
+    Person2("aaa") // main -> sub
 }
