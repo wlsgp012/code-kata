ab1604fa1798a6c1a8395ca38344eaca80129430
jinhye <wlsgp012@naver.com>
2022-04-03 09:35:55 +0900

    Add kotlin data structure sharing test
    export JAVA_HOME=/Users/user/.sdkman/candidates/java/11.0.2-open

---
+++ kata-kotlin/src/main/kotlin/dojo/datasharing/StructureSharingTest.kt
@@ -0,0 +1,13 @@
+package dojo.datasharing
+
+fun main() {
+    val x = listOf(1, 2, 3)
+    println(x)
+    val y = x + 4
+    println(y)
+    val z = y.dropLast(1)
+    println(z)
+
+    println(x == z)
+    println(x === z)
+}
