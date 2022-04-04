bdbd55396c4e0f94e21fa441b72e9951ba49527e
jinhye <wlsgp012@naver.com>
2022-04-04 17:23:44 +0900

    Add 4clojure 145

---
+++ kata-clojure/src/dojo/problems_in_4clojure/145_for_the_win.clj
@@ -0,0 +1,16 @@
+(ns dojo.problems-in-4clojure.145-for-the-win)
+;; https://4clojure.oxal.org/#/problem/145
+
+(def sol (take 10 (iterate #(+ 4 %) 1)))
+
+(= sol (for [x (range 40)
+            :when (= 1 (rem x 4))]
+        x))
+
+(= sol (for [x (iterate #(+ 4 %) 0)
+            :let [z (inc x)]
+            :while (< z 40)]
+        z))
+
+(= sol (for [[x y] (partition 2 (range 20))]
+        (+ x y)))
