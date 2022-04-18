841278dbfb6cf1c2bd5a07c7f94a6f5180b5fff6
jinhye <wlsgp012@naver.com>
2022-04-18 23:48:27 +0900

    Add 4clojure 96

--- kata-clojure/src/dojo/problems_in_4clojure/096_beauty_is_symmetry.clj
+++ kata-clojure/src/dojo/problems_in_4clojure/096_beauty_is_symmetry.clj
@@ -1,14 +1,14 @@
 (ns dojo.problems-in-4clojure.096-beauty-is-symmetry)
 ;; https://4clojure.oxal.org/#/problem/96
 
-(defn sym?
-  [a b]
-  (= a (reverse b)))
-
 (defn sol [xs]
-  (let [sc (second xs)
-        third (last xs)]
-    (= (second xs) (reverse (last xs)))))
+  (loop [l (rest xs)]
+    (let [firsts (map first l)
+          [a b] (split-at (/ (count firsts) 2) firsts)]
+      (cond
+        (not= a (reverse b)) false
+        (empty? (filter some? firsts)) true
+        :else (recur (reduce #(into %1 (rest %2)) [] l))))))
 
 (= (sol '(:a (:b nil nil) (:b nil nil))) true)
 
@@ -27,3 +27,21 @@
 (= (sol [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
          [2 [3 nil [4 [6 nil nil] nil]] nil]])
    false)
+
+;; others
+(fn s
+  ([[n a b]] (s a b))
+  ([[n1 a1 b1] [n2 a2 b2]]
+   (cond (and (nil? n1) (nil? n2)) true
+         (or (nil? n1) (nil? n2)) false
+         :else (and (= n1 n2) (s a1 b2) (s a2 b1)))))
+
+(fn [[n l r]]
+  (= l ((fn f [[n l r]]
+          (if n [n (f r) (f l)]))
+        r)))
+
+(fn symm? [x]
+  (= x ((fn mir [[v l r :as t]]
+          (when t
+            (conj [v] (mir r) (mir l)))) x)))
