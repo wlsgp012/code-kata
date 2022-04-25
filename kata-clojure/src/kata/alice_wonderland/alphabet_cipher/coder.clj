69c17f50dd07e7440881f5e7a6e9dd59a9f5405b
jinhye <wlsgp012@naver.com>
2022-04-25 22:01:03 +0900

    Add clojure kata alphabet cipher encode

--- kata-clojure/src/kata/alice_wonderland/alphabet_cipher/coder.clj
+++ kata-clojure/src/kata/alice_wonderland/alphabet_cipher/coder.clj
@@ -1,11 +1,20 @@
 (ns kata.alice-wonderland.alphabet-cipher.coder)
 ;; https://github.com/gigasquid/wonderland-clojure-katas/tree/master/alphabet-cipher
 
+(defn transform [c p]
+  (let [a (int \a)
+        z (int \z)
+        distance (- (int p) a)
+        r (+ (int c) distance)]
+    (char
+     (if (> r z)
+       (+ (- a 1) (- r z))
+       r))))
 
 (defn encode [keyword message]
-  )
+  (apply str (map transform message (cycle keyword))))
 
-( decode [keyword message]
+(defn decode [keyword message]
   "decodeme")
 
 (defn decipher [cipher message]
---
+++ kata-clojure/test/kata/alice_wonderland/alphabet_cipher/coder_test.clj
@@ -0,0 +1,27 @@
+(ns kata.alice-wonderland.alphabet-cipher.coder-test
+  (:require [kata.alice-wonderland.alphabet-cipher.coder :refer :all]
+            [clojure.test :refer :all]))
+
+
+(deftest test-encode
+  (testing "can encode a message with a secret keyword"
+    (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
+           (encode "vigilance" "meetmeontuesdayeveningatseven")))
+    (is (= "egsgqwtahuiljgs"
+           (encode "scones" "meetmebythetree")))))
+
+(deftest test-decode
+  (testing "can decode a message given an encoded message and a secret keyword"
+    (is (= "meetmeontuesdayeveningatseven"
+           (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")))
+    (is (= "meetmebythetree"
+           (decode "scones" "egsgqwtahuiljgs")))))
+
+(deftest test-decipher
+  (testing "can extract the secret keyword given an encrypted message and the original message"
+    (is (= "vigilance"
+           (decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog")))
+    (is (= "scones"
+           (decipher "hcqxqqtqljmlzhwiivgbsapaiwcenmyu" "packmyboxwithfivedozenliquorjugs")))
+    (is (= "abcabcx"
+           (decipher "hfnlphoontutufa" "hellofromrussia")))))
