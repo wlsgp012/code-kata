8561ba2c55870618cd861f8389c6cbdaef8829cf
jinhye <wlsgp012@naver.com>
2022-04-25 21:02:06 +0900

    Init clojure kata alice wonderland

---
+++ kata-clojure/src/kata/alice_wonderland/alphabet_cipher/coder.clj
@@ -0,0 +1,12 @@
+(ns kata.alice-wonderland.alphabet-cipher.coder)
+;; https://github.com/gigasquid/wonderland-clojure-katas/tree/master/alphabet-cipher
+
+
+(defn encode [keyword message]
+  )
+
+( decode [keyword message]
+  "decodeme")
+
+(defn decipher [cipher message]
+  "decypherme")
