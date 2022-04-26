71ce6cb981d24359533dbbeee14554290cc116c4
jinhye <wlsgp012@naver.com>
2022-04-26 23:11:21 +0900

    Add clojure kata alphabet cipher decode

--- kata-clojure/src/kata/alice_wonderland/alphabet_cipher/coder.clj
+++ kata-clojure/src/kata/alice_wonderland/alphabet_cipher/coder.clj
@@ -11,11 +11,21 @@
        (+ (- a 1) (- r z))
        r))))
 
+(defn r-transform [c p]
+  (let [a (int \a)
+        z (int \z)
+        distance (- (int p) a)
+        r (- (int c) distance)]
+    (char
+     (if (> a r)
+       (- (+ z 1) (- a r))
+       r))))
+
 (defn encode [keyword message]
   (apply str (map transform message (cycle keyword))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map r-transform message (cycle keyword))))
 
 (defn decipher [cipher message]
   "decypherme")
