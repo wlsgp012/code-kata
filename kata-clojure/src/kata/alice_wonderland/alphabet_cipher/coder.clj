2d335fe75fc04141e80e54a70ddc34fda2c6336d
jinhye <wlsgp012@naver.com>
2022-04-27 18:17:10 +0900

    Add clojure kata alphabet decipher

--- kata-clojure/src/kata/alice_wonderland/alphabet_cipher/coder.clj
+++ kata-clojure/src/kata/alice_wonderland/alphabet_cipher/coder.clj
@@ -1,11 +1,14 @@
 (ns kata.alice-wonderland.alphabet-cipher.coder)
 ;; https://github.com/gigasquid/wonderland-clojure-katas/tree/master/alphabet-cipher
 
+(defn alphabet-index-vector [start end]
+  (vec (range (int start) (inc (int end)))))
+
 (defn trans
   ([c p] (trans c p +))
   ([c p direction]
    (let [a (int \a)
-         v (vec (range a (inc (int \z))))
+         v (alphabet-index-vector \a \z)
          distance (- (int p) a)
          current-pos (- (int c) a)]
      (char (nth v (mod (direction current-pos distance) (count v)))))))
@@ -23,5 +26,16 @@
 (defn decode [keyword message]
   (coding keyword message rtrans))
 
+(defn rcycle [s]
+  (loop [length 1]
+    (when (> (count s) length)
+      (let [p (partition length s)]
+        (if (every? #(= (first p) %) p)
+          (apply str (first p))
+          (recur (inc length)))))))
+
 (defn decipher [cipher message]
-  "decypherme")
+  (let [v (alphabet-index-vector \a \z)]
+    (rcycle (apply str
+                   (map (fn [c m] (char (nth v (let [r (- (int c) (int m))] (if (neg? r) (+ r (count v)) r)))))
+                        cipher message)))))
