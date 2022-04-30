349d93b93078eb127c4d6e04c7c32979b0851549
jinhye <wlsgp012@naver.com>
2022-04-30 18:48:51 +0900

    Init clojure kata wonderland number

---
+++ kata-clojure/src/kata/alice_wonderland/wonderland_number/finder.clj
@@ -0,0 +1,7 @@
+(ns kata.alice-wonderland.wonderland-number.finder)
+;; https://github.com/gigasquid/wonderland-clojure-katas/tree/master/wonderland-number
+
+
+(defn wonderland-number []
+  ;; calculate me
+  42)
