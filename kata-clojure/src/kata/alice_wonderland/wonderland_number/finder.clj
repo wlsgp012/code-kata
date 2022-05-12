b1890e4be401aa55f6105417712800e41239ff17
jinhye <wlsgp012@naver.com>
2022-05-12 11:21:44 +0900

    Solve clojure kata wonderland number

--- kata-clojure/src/kata/alice_wonderland/wonderland_number/finder.clj
+++ kata-clojure/src/kata/alice_wonderland/wonderland_number/finder.clj
@@ -1,7 +1,19 @@
 (ns kata.alice-wonderland.wonderland-number.finder)
 ;; https://github.com/gigasquid/wonderland-clojure-katas/tree/master/wonderland-number
 
+(defn- to-digit-set [n]
+  (set (str n)))
+
+(defn- hasAllTheSameDigits? [a b]
+  (= (to-digit-set a) (to-digit-set b)))
+
+(defn- all-match? [n]
+  (and
+   (hasAllTheSameDigits? n (* 2 n))
+   (hasAllTheSameDigits? n (* 3 n))
+   (hasAllTheSameDigits? n (* 4 n))
+   (hasAllTheSameDigits? n (* 5 n))
+   (hasAllTheSameDigits? n (* 6 n))))
 
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (filter all-match? (range 100000 999999))))
