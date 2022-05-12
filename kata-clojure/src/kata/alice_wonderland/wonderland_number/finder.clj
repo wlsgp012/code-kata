5b1bd860bf277c55a21eadfd85ac87e7f76133e3
jinhye <wlsgp012@naver.com>
2022-05-12 14:14:47 +0900

    Solve clojure kata wonderland number(add others solutions)

--- kata-clojure/src/kata/alice_wonderland/wonderland_number/finder.clj
+++ kata-clojure/src/kata/alice_wonderland/wonderland_number/finder.clj
@@ -16,4 +16,24 @@
    (hasAllTheSameDigits? n (* 6 n))))
 
 (defn wonderland-number []
-  (first (filter all-match? (range 100000 999999))))
+  (first (filter all-match? (range 100000 1000000))))
+
+;; others
+(defn brute []
+  "Brute force solution to produce a sequence of six-digit numbers
+  whose products of 2, 3, 4, 5, and 6 all contain the same digits
+  as the number itself."
+  (for [n (range 100000 1000000)
+        :let [n2 (set (str (* n 2)))
+              n3 (set (str (* n 3)))
+              n4 (set (str (* n 4)))
+              n5 (set (str (* n 5)))
+              n6 (set (str (* n 6)))]
+        :when (= (set (str n)) n2 n3 n4 n5 n6)]
+    n))
+
+(defn wonderland-number []
+  (first (for [candidate (range 100000 166667)              ; 6 * 166666 is the last 6 digit number
+               :let [sorted-vals (map (comp sort str #(* candidate %)) (range 1 7))]
+               :when (apply = sorted-vals)]
+           candidate)))
