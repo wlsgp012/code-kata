ca07bfa895f2d05e00f4c8ec388fd951873636cc
jinhye <wlsgp012@naver.com>
2022-05-16 23:42:50 +0900

    Init clojure kata doublets

---
+++ kata-clojure/test/kata/alice_wonderland/doublets/solver_test.clj
@@ -0,0 +1,21 @@
+(ns kata.alice-wonderland.doublets.solver-test
+  (:require [kata.alice-wonderland.doublets.solver :refer :all]
+            [clojure.test :refer :all]))
+
+(deftest solver-test
+  (testing "with word links found"
+    (is (= ["head" "heal" "teal" "tell" "tall" "tail"]
+           (doublets "head" "tail")))
+
+    (is (= ["door" "boor" "book" "look" "lock"]
+           (doublets "door" "lock")))
+
+    (is (= ["bank" "bonk" "book" "look" "loon" "loan"]
+           (doublets "bank" "loan")))
+
+    (is (= ["wheat" "cheat" "cheap" "cheep" "creep" "creed" "breed" "bread"]
+           (doublets "wheat" "bread"))))
+
+  (testing "with no word links found"
+    (is (= []
+           (doublets "ye" "freezer")))))
