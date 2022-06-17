fa177a5ffd47f70e496fff4bf08c816dad146a43
jinhye <wlsgp012@naver.com>
2022-06-17 15:52:59 +0900

    Add book joy of kotlin p.161

--- kata-kotlin/src/main/kotlin/dojo/joyofkotlin/ch4/Recursive.kt
+++ kata-kotlin/src/main/kotlin/dojo/joyofkotlin/ch4/Recursive.kt
@@ -1,5 +1,7 @@
 package dojo.joyofkotlin.ch4
 
+import java.math.BigInteger
+
 fun hello() {
     println("Hello")
     hello()
@@ -25,11 +27,36 @@ fun sum(n: Int): Int {
  */
 fun inc(n: Int) = n + 1
 fun dec(n: Int) = n - 1
-fun add(a: Int, b: Int): Int = if(b == 0) a else add(inc(a), dec(b))
+fun add(a: Int, b: Int): Int = if (b == 0) a else add(inc(a), dec(b))
+
+/**
+ * p.161 4-2
+ */
+object Factorial {
+    val factorial: (Int) -> Int by lazy {
+        { n: Int -> if (n <= 1) n else n * factorial(n - 1) }
+    }
+}
+
+fun fact(n: Int): BigInteger {
+    tailrec fun fact(x: BigInteger, r: BigInteger): BigInteger = if (x > n.toBigInteger()) r else fact(x + BigInteger.ONE, r * x)
+    return fact(BigInteger.ONE, BigInteger.ONE)
+}
+
+fun f(x: BigInteger, r: BigInteger): BigInteger{
+    return x + r
+}
 
 fun main() {
 //    hello()
 //    println(sum_old(10))
 //    println(sum(10))
-    println( add(4, 3))
+//    println(add(4, 3))
+//    println(fact(0))
+//    println(fact(1))
+//    println(fact(2))
+//    println(fact(3))
+//    println(fact(4))
+//    println(fact(100000))
+    println(Factorial.factorial(4))
 }
