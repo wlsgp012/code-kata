5fd75494bee157d63ce0970352bd48519e33de3f
jinhye <wlsgp012@naver.com>
2022-08-24 15:14:34 +0900

    Solve clojure kata wonderland doublets - 1 test failed

--- kata-clojure/src/kata/alice_wonderland/doublets/solver.clj
+++ kata-clojure/src/kata/alice_wonderland/doublets/solver.clj
@@ -12,5 +12,26 @@
                (slurp)
                (read-string)))
 
-(defn doublets [word1 word2]
-  "make me work")
+(defn equal-word-except-one-letter
+  [a b]
+  (if (= (count a) (count b))
+    (= 1 (count (filter false? (map = a b))))
+    false))
+
+(defn same-lengths
+  [length l]
+  (filter #(= (count %) length) l))
+
+(defn pick-next
+  [prev words results]
+  (let [rs (set results)]
+    (first (filter #(not (contains? rs %)) (filter (partial equal-word-except-one-letter prev) words)))))
+
+(defn doublets
+  ([word1 word2]
+   (doublets word1 word2 words []))
+  ([word1 word2 words result]
+   (let [next (pick-next word1 words result)]
+     (if (nil? next)
+       (if (not= word1 word2) [] (conj result word1))
+       (recur next word2 (remove #(= next %) words) (conj result word1))))))
