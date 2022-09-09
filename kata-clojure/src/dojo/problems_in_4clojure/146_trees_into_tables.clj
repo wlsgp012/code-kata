5f231dc0fa48845971fc646fb9becd0c9f31fefc
jinhye <wlsgp012@naver.com>
2022-09-09 16:56:25 +0900

    Add 4clojure 146

---
+++ kata-clojure/src/dojo/problems_in_4clojure/146_trees_into_tables.clj
@@ -0,0 +1,28 @@
+(ns dojo.problems-in-4clojure.146-trees-into-tables)
+
+(def sol
+  (fn
+    ([m] (sol m []))
+    ([m super-keys]
+     (reduce (fn [r [k v]]
+               (let [karr (conj super-keys k)]
+                 (into r (if (not (map? v)) [{karr v}] (sol v karr)))))
+             {}
+             m))))
+
+(= (sol '{a {p 1, q 2}
+         b {m 3, n 4}})
+   '{[a p] 1, [a q] 2
+     [b m] 3, [b n] 4})
+
+(= (sol '{[1] {a b c d}
+         [2] {q r s t u v w x}})
+   '{[[1] a] b, [[1] c] d,
+     [[2] q] r, [[2] s] t,
+     [[2] u] v, [[2] w] x})
+
+(= (sol '{m {1 [a b c] 3 nil}})
+   '{[m 1] [a b c], [m 3] nil})
+
+;; others
+;; tbu
