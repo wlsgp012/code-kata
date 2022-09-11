16f83e42b63934099aa4ad9a74c8017325feb172
jinhye <wlsgp012@naver.com>
2022-09-11 17:53:32 +0900

    Add hanoi: print disc number & not use 'else'

--- kata-clojure/src/dojo/hanoi/simple.clj
+++ kata-clojure/src/dojo/hanoi/simple.clj
@@ -1,15 +1,23 @@
 (ns dojo.hanoi.simple)
 
-(defn hanoi
-  ([n] (hanoi n 1 3 2))
+(defn hanoi2
+  ([n] (hanoi2 n 1 3 2))
   ([n from to empty]
    (let [moving (str from ">>>" to)]
      (if (= n 1)
        (println moving)
        (doall
-        [(hanoi (dec n) from empty to)
+        [(hanoi2 (dec n) from empty to)
          (println moving)
-         (hanoi (dec n) empty to from)])))))
+         (hanoi2 (dec n) empty to from)])))))
+
+(defn hanoi
+  ([n] (hanoi n 1 3 2))
+  ([n from to empty]
+             (when (> n 0)
+               (hanoi (dec n) from empty to)
+               (println (str "Move " n " from " from " to " to))
+               (hanoi (dec n) empty to from))))
 
 (comment
-  (hanoi 3))
+  (hanoi 2))
--- kata-python/algorithms/hanoi/simple.py
+++ kata-python/algorithms/hanoi/simple.py
@@ -1,9 +1,16 @@
-def hanoi(n, frm=1, to=3, empty=2):
+def hanoi2(n, frm=1, to=3, empty=2):
     if n == 1:
         print(f'{frm}>>{to}')
     else:
-        hanoi(n - 1, frm, empty, to)
+        hanoi2(n - 1, frm, empty, to)
         print(f'{frm}>>{to}')
+        hanoi2(n - 1, empty, to, frm)
+
+
+def hanoi(n, frm=1, to=3, empty=2):
+    if n > 0:
+        hanoi(n - 1, frm, empty, to)
+        print(f'disc={n},{frm}>>{to}')
         hanoi(n - 1, empty, to, frm)
 
 
