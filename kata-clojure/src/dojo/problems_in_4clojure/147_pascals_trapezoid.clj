d273616c98f0a6718792d082ae771366c0225136
jinhye <wlsgp012@naver.com>
2022-09-12 14:18:49 +0900

    Add 4clojure 147

---
+++ kata-clojure/src/dojo/problems_in_4clojure/147_pascals_trapezoid.clj
@@ -0,0 +1,26 @@
+(ns dojo.problems-in-4clojure.147-pascals-trapezoid)
+
+(defn sol [v]
+  (letfn [(append-zero [vv] (conj vv 0))
+          (prepend-zero [vv] (into [0] vv))
+          (combine [x y] (vec (map +' x y)))]
+    (iterate #(combine (append-zero %) (prepend-zero %)) v)))
+
+(= (second (sol [2 3 2])) [2 5 5 2])
+
+(= (take 5 (sol [1])) [[1] [1 1] [1 2 1] [1 3 3 1] [1 4 6 4 1]])
+
+(= (take 2 (sol [3 1 2])) [[3 1 2] [3 4 3 2]])
+
+(= (take 100 (sol [2 4 2])) (rest (take 101 (sol [2 2]))))
+
+
+;; others
+
+(fn [init]
+  (iterate #(map +'
+                 (concat [0] %)
+                 (concat % [0]))
+           init))
+
+#(iterate (fn [xs] (map +' `(0 ~@xs) `(~@xs 0))) %)
