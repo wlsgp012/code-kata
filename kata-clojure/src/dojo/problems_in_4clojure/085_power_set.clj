85f542335f1a35780ef0bae353e43148f10f25af
jinhye <wlsgp012@naver.com>
2022-09-13 12:30:34 +0900

    Solve 4clojure 085

--- kata-clojure/src/dojo/problems_in_4clojure/085_power_set.clj
+++ kata-clojure/src/dojo/problems_in_4clojure/085_power_set.clj
@@ -2,8 +2,16 @@
 ;; https://4clojure.oxal.org/#/problem/85
 
 (defn sol
-  [s]
-  )
+  ([s] (sol s 0 #{#{}}))
+  ([s size result]
+   (if (= size (count s))
+     result
+     (recur s (inc size)
+            (into result
+                  (for [x result
+                        y s
+                        :when (= size (count x))]
+                    (conj x y)))))))
 
 (= (sol #{1 :a}) #{#{1 :a} #{:a} #{} #{1}})
 
@@ -13,3 +21,17 @@
    #{#{} #{1} #{2} #{3} #{1 2} #{1 3} #{2 3} #{1 2 3}})
 
 (= (count (sol (into #{} (range 10)))) 1024)
+
+;; others
+(fn [xs]
+  (letfn [(powerset [[x & xs]]
+            (if x
+              (concat (map (partial cons x) (powerset xs)) (powerset xs))
+              [[]]))]
+    (->> (powerset (into [] xs))
+         (map set)
+         set)))
+
+(fn power-set
+  [s]
+  (reduce #(into % (for [subset %] (conj subset %2))) #{#{}} s))
