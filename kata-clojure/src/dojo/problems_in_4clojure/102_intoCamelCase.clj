f97d2e6816583d718650ee79ce3e243b8816db0d
jinhye <wlsgp012@naver.com>
2022-09-27 23:43:26 +0900

    Init 4clojure 102

---
+++ kata-clojure/src/dojo/problems_in_4clojure/102_intoCamelCase.clj
@@ -0,0 +1,11 @@
+(ns dojo.problems-in-4clojure.102-intoCamelCase)
+
+(def sol
+  (fn [word]
+    ))
+
+(= (sol "something") "something")
+
+(= (sol "multi-word-key") "multiWordKey")
+
+(= (sol "leaveMeAlone") "leaveMeAlone")
