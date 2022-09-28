f00d84c89138af5ed80aa2070fee8d79836ececb
jinhye <wlsgp012@naver.com>
2022-09-28 23:44:10 +0900

    Solve 4clojure 102

--- kata-clojure/src/dojo/problems_in_4clojure/102_intoCamelCase.clj
+++ kata-clojure/src/dojo/problems_in_4clojure/102_intoCamelCase.clj
@@ -2,10 +2,18 @@
 
 (def sol
   (fn [word]
-    ))
+    (let [separated (re-seq #"\w+" word)]
+      (clojure.string/join (cons (first separated) (map clojure.string/capitalize (rest separated)))))))
 
 (= (sol "something") "something")
 
 (= (sol "multi-word-key") "multiWordKey")
 
 (= (sol "leaveMeAlone") "leaveMeAlone")
+
+;; others
+(fn [str] (clojure.string/replace str #"-\w" #(-> % (.substring 1 2) .toUpperCase)))
+
+(fn [s]
+  (let [[h & t] (clojure.string/split s #"\-")]
+    (clojure.string/join (cons h (map clojure.string/capitalize t)))))
