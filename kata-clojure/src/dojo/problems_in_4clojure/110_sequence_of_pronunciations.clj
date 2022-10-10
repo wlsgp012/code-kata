fa42648cbe60a98db9c7dd65104120b5d66e3cab
jinhye <wlsgp012@naver.com>
2022-10-10 23:00:45 +0900

    Solve 4clojure 110

--- kata-clojure/src/dojo/problems_in_4clojure/110_sequence_of_pronunciations.clj
+++ kata-clojure/src/dojo/problems_in_4clojure/110_sequence_of_pronunciations.clj
@@ -1,6 +1,8 @@
 (ns dojo.problems-in-4clojure.110-sequence-of-pronunciations)
 
-(defn sol [v])
+(defn sol [v]
+  (let [r (mapcat #(vector (count %) (first %)) (partition-by identity v))]
+    (cons r (lazy-seq (sol r)))))
 
 (= [[1 1] [2 1] [1 2 1 1]] (take 3 (sol [1])))
 
@@ -9,3 +11,8 @@
 (= [1 1 1 3 2 1 3 2 1 1] (nth (sol [1]) 6))
 
 (= 338 (count (nth (sol [3 2]) 15)))
+
+;; others
+(fn pronounce [xs]
+  (let [pron (fn [xs] (mapcat (fn [x] [(count x) (first x)]) (partition-by identity xs)))]
+    (iterate pron (pron xs))))
