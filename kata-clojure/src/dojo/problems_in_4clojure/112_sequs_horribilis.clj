ba5c2a547c6fc74fba5279485910f2df0e143df3
jinhye <wlsgp012@naver.com>
2022-10-12 18:49:28 +0900

    Add solving 4clojure 112

--- kata-clojure/src/dojo/problems_in_4clojure/112_sequs_horribilis.clj
+++ kata-clojure/src/dojo/problems_in_4clojure/112_sequs_horribilis.clj
@@ -1,5 +1,16 @@
 (ns dojo.problems-in-4clojure.112-sequs-horribilis)
 
+;;wrong
+(defn sol-wrong [n coll]
+  (loop [sum 0 xs coll result []]
+    (let [es (take-while number? xs)
+          next-seq (drop-while number? xs)
+          next-sums (reduce #(if (>= n (+ (apply + %1) %2)) (conj %1 %2) %1) [] es)
+          next-sum (apply + next-sums)]
+      (if  (> n (+ sum next-sum))
+        (recur (+ sum next-sum) next-seq (vector (assoc result (count result) (conj (last result) (conj next-sums [])))))
+        (seq result)))))
+
 (defn sol [n coll]
   )
 
@@ -7,7 +18,7 @@
 
 (=  (sol 30 [1 2 [3 [4 [5 [6 [7 8]] 9]] 10] 11])    '(1 2 (3 (4 (5 (6 (7)))))))
 
-(=  (sol 9 (range))    '(0 1 2 3))
+(comment (=  (sol 9 (range))    '(0 1 2 3)))
 
 (=  (sol 1 [[[[[1]]]]])    '(((((1))))))
 
