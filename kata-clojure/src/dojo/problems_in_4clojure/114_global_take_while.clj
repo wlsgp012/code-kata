cbfc39e50286d0462bd9e80029a85eaf980706af
jinhye <wlsgp012@naver.com>
2022-10-20 21:02:21 +0900

    Solve 4clojure 114

--- kata-clojure/src/dojo/problems_in_4clojure/114_global_take_while.clj
+++ kata-clojure/src/dojo/problems_in_4clojure/114_global_take_while.clj
@@ -1,7 +1,17 @@
 (ns dojo.problems-in-4clojure.114-global-take-while)
 
-(defn sol [n p s]
-  )
+;; not lazy
+(defn sol2 [n p s]
+  (loop [c n [x & xs] s r []]
+    (if (zero? c)
+      (drop-last r)
+      (let [b (p x)]
+        (recur (if b (dec c) c) xs (conj r x))))))
+
+(defn sol [n p [x & xs]]
+  (let [c (if (p x) (dec n) n)]
+    (when (not (zero? c))
+      (cons x (sol c p xs)))))
 
 (= [2 3 5 7 11 13]
    (sol 4 #(= 2 (mod % 3))
@@ -13,4 +23,16 @@
 
 (= ["this" "is"]
    (sol 1 #{"a"}
-       ["this" "is" "a" "sentence" "i" "wrote"]))
+        ["this" "is" "a" "sentence" "i" "wrote"]))
+
+;; others
+(fn take-n-while [i pred coll]
+  (let [[f [m & r]] (split-with (complement pred) coll)]
+    (if (= 1 i)
+      f
+      (concat f [m] (take-n-while (dec i) pred r)))))
+
+(fn [n prd col]
+  (let [a (atom 0)]
+    (take-while
+     (fn [x] (if (prd x) (swap! a inc)) (< @a n)) col)))
