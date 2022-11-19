d286d661e9bf26c61e4b98ef85f240d59d4dbc64
jinhye <wlsgp012@naver.com>
2022-11-20 02:24:07 +0900

    Add solving 150

--- kata-clojure/src/dojo/problems_in_4clojure/150_palindromic_number.clj
+++ kata-clojure/src/dojo/problems_in_4clojure/150_palindromic_number.clj
@@ -1,25 +1,46 @@
 (ns dojo.problems-in-4clojure.150-palindromic-number)
 
-(defn sol [n]
-  [])
+;; slow
+(defn sol2 [x]
+  (let [as-string (str x)
+        palindrome? (= (list* as-string) (reverse as-string))
+        n (lazy-seq (sol2 (inc x)))]
+    (if palindrome?
+      (cons x n)
+      n)))
 
-(= (take 26 (sol 0))
-   [0 1 2 3 4 5 6 7 8 9
-    11 22 33 44 55 66 77 88 99
-    101 111 121 131 141 151 161])
-(= (take 16 (sol 162))
-   [171 181 191 202
-    212 222 232 242
-    252 262 272 282
-    292 303 313 323])
-(= (take 6 (sol 1234550000))
-   [1234554321 1234664321 1234774321
-    1234884321 1234994321 1235005321])
-(= (first (sol (* 111111111 111111111)))
-   (* 111111111 111111111))
-(= (set (take 199 (sol 0)))
-   (set (map #(first (sol %)) (range 0 10000))))
-(= true
-   (apply < (take 6666 (sol 9999999))))
-(= (nth (sol 0) 10101)
-   9102019)
+(defn sol [x]
+  (let [palindrome? (fn c [xs] (if (seq xs)
+                                 (and (= (first xs) (last xs)) (c (drop-last (rest xs))))
+                                 true))
+        n (lazy-seq (sol (inc x)))]
+    (if (palindrome? (str x))
+      (cons x n)
+      n)))
+
+(comment (= (take 26 (sol 0))
+            [0 1 2 3 4 5 6 7 8 9
+             11 22 33 44 55 66 77 88 99
+             101 111 121 131 141 151 161])
+
+         (= (take 16 (sol 162))
+            [171 181 191 202
+             212 222 232 242
+             252 262 272 282
+             292 303 313 323])
+
+         (= (take 6 (sol 1234550000))
+            [1234554321 1234664321 1234774321
+             1234884321 1234994321 1235005321])
+
+         (= (first (sol (* 111111111 111111111)))
+            (* 111111111 111111111))
+
+         (= (set (take 199 (sol 0)))
+            (set (map #(first (sol %)) (range 0 10000))))
+
+         (= true
+            (apply < (take 6666 (sol 9999999))))
+
+         (= (nth (sol 0) 10101)
+            9102019))
