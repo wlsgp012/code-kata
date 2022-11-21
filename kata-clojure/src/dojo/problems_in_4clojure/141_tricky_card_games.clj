3bf1d8510f8c340203a63777b3df50d97ed43f7b
jinhye <wlsgp012@naver.com>
2022-11-22 00:53:13 +0900

    Fix answer of 4clojure 141

--- kata-clojure/src/dojo/problems_in_4clojure/141_tricky_card_games.clj
+++ kata-clojure/src/dojo/problems_in_4clojure/141_tricky_card_games.clj
@@ -1,18 +1,26 @@
 (ns dojo.problems-in-4clojure.141-tricky-card-games)
 
-(defn sol [trump-suit]
+;; not exactly
+(defn sol2 [trump-suit]
   (fn [cards]
     (letfn [(filter-by-suit [{suit :suit}] (if (nil? trump-suit) true (= suit trump-suit)))
             (sort-by-suit [{suit :suit}] (.indexOf [:spade :heart :diamond :club] suit))]
       (first (sort-by (juxt sort-by-suit (comp - :rank)) (filter filter-by-suit cards))))))
+
+(defn sol [trump-suit]
+  (fn [cards]
+    (letfn [(filter-by-suit [{suit :suit}] (if (nil? trump-suit) (= suit (:suit (first cards))) (= suit trump-suit)))]
+      (first (sort-by (comp - :rank) (filter filter-by-suit cards))))))
 
 (let [notrump (sol nil)]
   (and (= {:suit :club :rank 9}  (notrump [{:suit :club :rank 4}
                                            {:suit :club :rank 9}]))
        (= {:suit :spade :rank 2} (notrump [{:suit :spade :rank 2}
                                            {:suit :club :rank 10}]))))
+
 (= {:suit :club :rank 10} ((sol :club) [{:suit :spade :rank 2}
-                                       {:suit :club :rank 10}]))
+                                        {:suit :club :rank 10}]))
+
 (= {:suit :heart :rank 8}
    ((sol :heart) [{:suit :heart :rank 6} {:suit :heart :rank 8}
                  {:suit :diamond :rank 10} {:suit :heart :rank 4}]))
