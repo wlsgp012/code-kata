ffd507c783a75b398c61b30e6ec210f891805722
jinhye <wlsgp012@naver.com>
2023-04-14 11:09:22 +0900

    Add book joy of kotlin p.317 8-9

--- kata-kotlin/src/main/kotlin/dojo/joyofkotlin/ch8/list.kt
+++ kata-kotlin/src/main/kotlin/dojo/joyofkotlin/ch8/list.kt
@@ -25,4 +25,32 @@ fun <A> flattenResult(list: List<Result<A>>): List<A> {
  * p.313 8-6
  */
 fun <A> sequence(list: List<Result<A>>): Result<List<A>> =
-    list.foldRight(Result(List())){ ra -> { acc -> map2(ra, acc){ a -> { xs -> xs.cons(a)}} }}
+    list.foldRight(Result(List())) { ra -> { acc -> map2(ra, acc) { a -> { xs -> xs.cons(a) } } } }
+
+/**
+ * p.314 8-7
+ */
+fun <A, B> traverse(list: List<A>, f: (A) -> Result<B>): Result<List<B>> =
+    list.foldRight(Result(List())) { a -> { acc -> map2(f(a), acc) { a -> { xs -> xs.cons(a) } } } }
+
+fun <A> sequence_via_traverse(list: List<Result<A>>): Result<List<A>> = traverse(list) { it }
+
+/**
+ * p.315 8-8
+ */
+fun <A, B, C> zipWith(xs: List<A>, ys: List<B>, f: (A) -> (B) -> C): List<C> {
+    tailrec fun process(result: List<C>, xss: List<A>, yss: List<B>): List<C> =
+        when (xss) {
+            is List.Empty -> result
+            is List.Cons -> when (yss) {
+                is List.Empty -> result
+                is List.Cons -> process(result.cons(f(xss.head)(yss.head)), xss.tail, yss.tail)
+            }
+        }
+    return process(List(), xs, ys).reverse()
+}
+
+/**
+ * p.317 8-9
+ */
+fun <A, B, C> product(xs: List<A>, ys: List<B>, f: (A) -> (B) -> C): List<C> = xs.flatMap { x -> ys.map{ y -> f(x)(y)} }
