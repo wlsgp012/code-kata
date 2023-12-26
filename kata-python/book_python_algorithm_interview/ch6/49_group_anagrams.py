736c5684ebcbde8bc3230b5074e0abb622f1c3f7
jinhye <wlsgp012@naver.com>
2023-12-26 17:37:08 +0900

    Formatting

--- kata-python/book_python_algorithm_interview/ch6/49_group_anagrams.py
+++ kata-python/book_python_algorithm_interview/ch6/49_group_anagrams.py
@@ -2,17 +2,20 @@
 import collections
 from functools import reduce
 
-strs = ["eat","tea","tan","ate","nat","bat"]
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
 
 def to_group(xs: list[str]) -> list[list[str]]:
     d = reduce(to_map, xs, collections.defaultdict(list))
-        return d.values()
+    return d.values()
+
 
 def to_map(m: dict[str, list[str]], x: str):
     k = "".join(sorted(x))
-    xs = m[ k ]
+    xs = m[k]
     xs.append(x)
     return m
+
 
 # in book
 def answer(strs):
